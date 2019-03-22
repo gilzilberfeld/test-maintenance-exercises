ca0b6617a58579ff2e1aa4921c30ce56e2fb6623
gilzilberfeld <gil.zilberfeld@gmail.com>
2019-03-22 14:54:46 +0200

    Added Cucumber test for C#

---
+++ CSharp/CalculatorServerTests/CucumberTests/CucumberCalculatorTests.feature
@@ -0,0 +1,10 @@
+﻿Feature: Calculator 
+This feature checks the calculator does calculations correctly
+
+Scenario: 1 plus 2 equals 3
+Given A reset calculator 
+When The user presses '1'
+And The user presses '+'	
+And The user presses '2'
+And The user presses '='	
+Then The calculator displays '3'
---
+++ CSharp/CalculatorServerTests/CucumberTests/CucumberTestsStepDefinitions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+namespace CalculatorServerTests
+{
+    [FeatureFile("CucumberTests\\CucumberCalculatorTests.feature")]
+    public class CucumberTestsStepDefinitions : Feature, IDisposable
+    {
+        private TestServerFactory factory;
+        private HttpClient client;
+
+
+        public CucumberTestsStepDefinitions() : base()
+        {
+            factory = new TestServerFactory();
+            client = factory.CreateClient();
+        }
+
+        [Given(@"A reset calculator")]
+        public async Task GivenAResetCalculator()
+        {
+            await client.PostAsync("calculator/reset", null);
+        }
+
+        [When(@"The user presses (.*)")]
+        [And(@"The user presses (.*)")]
+        public async Task WhenTheUserPresses(string key)
+        {
+            string url = "calculator/press?key=" + GetEncodedKey(key);
+            await client.PostAsync(url, null);
+        }
+
+        [Then(@"The calculator displays (.*)")]
+        public async Task ThenTheCalculatorDisplays(string display)
+        {
+            var response = await client.GetAsync("calculator/display");
+            Assert.Equal("3", response.Content.ReadAsStringAsync().Result);
+        }
+
+        private static string GetEncodedKey(string key)
+        {
+            string realKey = key.Substring(1, 1);
+            return HttpUtility.UrlEncode(realKey);
+        }
+
+
+        public void Dispose()
+        {
+            factory.Dispose();
+            client.Dispose();
+        }
+
+    }
+}
